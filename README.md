c155c0d39202a4db604c159ef2218a7dd0229cda
Wenzel Jakob <wenzel@inf.ethz.ch>
2015-02-09 18:36:46 +0100

    new website

---
+++ README.md
@@ -0,0 +1,9 @@
+## Nori Version 2
+
+Nori is a simple ray tracer written in C++. It runs on Windows, Linux, and
+Mac OS and provides basic functionality that is required to complete the
+assignments in the course Image Synthesis taught at ETH Zürich.
+
+### Course information and framework documentation
+
+For access to course information including slides and reading material, visit the main [Image Synthesis website] (https://graphics.ethz.ch/teaching/imsynth14/home.php). The Nori 2 framework and coding assignments will be described on the [Nori website] (https://wjakob.github.io/nori2).
---
+++ js/nori.js
@@ -0,0 +1,24 @@
+!function ($) {
+	$('a[href*=#]').on('click', function(event){     
+		$('html,body').animate({scrollTop:$(this.hash + "-header").offset().top}, 500);
+	});
+
+	prettify = function() {
+		$("code").addClass("prettyprint");
+		$("code").addClass("lang-cpp");
+		prettyPrint();
+		MathJax.Hub.Typeset();
+	};
+
+	$("#prelim-body").load("prelim.html", prettify);
+	$("#pa1-body").load("pa1.html", prettify);
+	$("#pa2-body").load("pa2.html", prettify);
+	$("#pa3-body").load("pa3.html", prettify);
+	$("#pa4-body").load("pa4.html", prettify);
+	$("#project-body").load("project.html", prettify);
+
+	$(".fancybox").fancybox();
+
+	$('.tooltips').tooltip({ selector: "span[rel=tooltip]" })
+}(window.jQuery)
+
