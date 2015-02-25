635c8bc82d4962838e4c4cd3e2965382722ae3c8
Wenzel Jakob <wenzel@inf.ethz.ch>
2015-02-25 19:31:35 +0100

    warping GUI

--- include/nori/warp.h
+++ include/nori/warp.h
@@ -28,10 +28,55 @@ NORI_NAMESPACE_BEGIN
 class Warp {
 public:
     /// Uniformly sample a vector on the unit hemisphere with respect to solid angles (naive implementation)
-    static Vector3f sampleUniformHemisphere(Sampler * sampler, const Normal3f & northPole);
+    static Vector3f sampleUniformHemisphere(Sampler *sampler, const Normal3f &northPole);
 
+    /// Dummy warping function: takes uniformly distributed points in a square and just returns them
+    static Point2f squareToUniformSquare(const Point2f &sample);
 
-    /* TBD: add more in programming assignment 2 */
+    /// Probability density of \ref squareToUniformSquare()
+    static float squareToUniformSquarePdf(const Point2f &p);
+
+    /// Uniformly sample a vector on a 2D disk with radius 1, centered around the origin
+    static Point2f squareToUniformDisk(const Point2f &sample);
+
+    /// Probability density of \ref squareToUniformDisk()
+    static float squareToUniformDiskPdf(const Point2f &p);
+
+    /// Uniformly sample a vector on the unit sphere with respect to solid angles
+    static Vector3f squareToUniformSphere(const Point2f &sample);
+
+    /// Probability density of \ref squareToUniformSphere()
+    static float squareToUniformSpherePdf(const Vector3f &v);
+
+    /**
+     * \brief Uniformly sample a vector on a spherical cap around (0, 0, 1)
+     *
+     * A spherical cap is the subset of a unit sphere whose directions
+     * make an angle of less than 'theta' with the north pole. This function
+     * expects the cosine of 'theta' as a parameter.
+     */
+    static Vector3f squareToUniformSphereCap(const Point2f &sample, float cosThetaMax);
+
+    /// Probability density of \ref squareToUniformSphereCap()
+    static float squareToUniformSphereCapPdf(const Vector3f &v, float cosThetaMax);
+
+    /// Uniformly sample a vector on the unit hemisphere around the pole (0,0,1) with respect to solid angles
+    static Vector3f squareToUniformHemisphere(const Point2f &sample);
+
+    /// Probability density of \ref squareToUniformHemisphere()
+    static float squareToUniformHemispherePdf(const Vector3f &v);
+
+    /// Uniformly sample a vector on the unit hemisphere around the pole (0,0,1) with respect to projected solid angles
+    static Vector3f squareToCosineHemisphere(const Point2f &sample);
+
+    /// Probability density of \ref squareToCosineHemisphere()
+    static float squareToCosineHemispherePdf(const Vector3f &v);
+
+    /// Warp a uniformly distributed square sample to a Beckmann distribution * cosine for the given 'alpha' parameter
+    static Vector3f squareToBeckmann(const Point2f &sample, float alpha);
+
+    /// Probability density of \ref squareToBeckmann()
+    static float squareToBeckmannPdf(const Vector3f &m, float alpha);
 };
 
 NORI_NAMESPACE_END
