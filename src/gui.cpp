50ba6527afeba25fa1d1224a4a94e4b3b968374c
Baptiste Nicolet <baptiste.nicolet@epfl.ch>
2021-06-23 10:14:08 +0200

    License changes

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,3 +1,9 @@
+/*
+    This file is part of Nori, a simple educational ray tracer
+
+    Copyright (c) 2015 by Wenzel Jakob
+*/
+
 #include <nori/gui.h>
 #include <nori/block.h>
 #include <nanogui/shader.h>
